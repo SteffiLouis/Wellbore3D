171ebc9563b2d64708b4a053ad1188aa4a8e88fe
eugin <eugin.bernads@shellsquare.com>
2019-10-17 20:13:39 +0530

    updated

--- js/trajectory.js
+++ js/trajectory.js
@@ -5,31 +5,56 @@ var renderer;
 init();
 
 function init() {
-    trajectorySurface = document.getElementById("trajectory_3d");
-    scene = new THREE.Scene();
+  trajectorySurface = document.getElementById("trajectory_3d");
+  scene = new THREE.Scene();
 
-    var aspectRatio = trajectorySurface.offsetWidth / trajectorySurface.offsetHeight;
-    camera = new THREE.PerspectiveCamera( 75, aspectRatio, .1, 1000 );
-    scene.add( camera );
+  var aspectRatio = trajectorySurface.offsetWidth / trajectorySurface.offsetHeight;
+  camera = new THREE.PerspectiveCamera(75, aspectRatio, .1, 1000);
+  scene.add(camera);
 
-    renderer = new THREE.WebGLRenderer({ canvas: trajectorySurface });
-    renderer.setSize(trajectorySurface.offsetWidth, trajectorySurface.offsetHeight);
-    renderer.setClearColor(0xffffff, 1);
+  renderer = new THREE.WebGLRenderer({
+    canvas: trajectorySurface
+  }, {
+    antialias: true
+  });
+  renderer.setSize(trajectorySurface.offsetWidth, trajectorySurface.offsetHeight);
+  renderer.setClearColor("#ddd", 1);
 
-    var controls = new THREE.OrbitControls(camera);
-    controls.mouseButtons = {
-        ORBIT: THREE.MOUSE.RIGHT,
-        ZOOM: THREE.MOUSE.MIDDLE,
-        PAN: THREE.MOUSE.LEFT
-    };
+  var controls = new THREE.OrbitControls(camera);
+  controls.mouseButtons = {
+    ORBIT: THREE.MOUSE.RIGHT,
+    ZOOM: THREE.MOUSE.MIDDLE,
+    PAN: THREE.MOUSE.LEFT
+  };
 
-    var ah = new THREE.AxesHelper(50);
-    ah.position.y -= 0.1;  // The axis helper should not intefere with the grid helper
-    scene.add(ah);
-    camera.position.x = 20;
-    camera.position.y = 30;
-    camera.position.z = 70;
-    render();
+ 
+  // Floor
+  var floorGeometry = new THREE.PlaneGeometry(80,100,0);
+  var floorMaterial = new THREE.MeshPhongMaterial({
+    color: "#E2B822",
+    shininess: 100
+  });
+  var floor = new THREE.Mesh(floorGeometry, floorMaterial);
+  floor.rotation.x = -0.5 * Math.PI;
+  floor.position.x=10;
+  floor.receiveShadow = true;
+  scene.add(floor);
+  var spotLight = new THREE.SpotLight(0xffffff);
+  spotLight.position.set(15, 30, 50);
+  // spotLight.castShadow = true;
+  scene.add(spotLight);
+  let ambientLight = new THREE.AmbientLight(0x0c0c0c);
+  scene.add(ambientLight);
+  var light = new THREE.PointLight(0xffffff, 0.5);
+  light.position.set(20, 20, 50);
+  scene.add(light);
+  var ah = new THREE.AxesHelper(50);
+  ah.position.y -= 0.1; // The axis helper should not intefere with the grid helper
+  scene.add(ah);
+  camera.position.x = 20;
+  camera.position.y = 30;
+  camera.position.z = 70;
+  render();
 };
 
 function render() {
@@ -39,7 +64,7 @@ function render() {
   controls.rotateSpeed = 0.001; // Rotate speed
   controls.autoRotate = false; // turn this guy to true for a spinning camera
   controls.autoRotateSpeed = 0.001; // 30
-  controls.maxPolarAngle = Math.PI / 4; // Don't let to go below the ground
+  controls.maxPolarAngle = Math.PI / 2; // Don't let to go below the ground
   requestAnimationFrame(render);
   controls.update();
   renderer.render(scene, camera);
