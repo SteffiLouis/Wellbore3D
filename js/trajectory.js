d4307bd5a63eed973c21121b2d097fb1314e5ca3
eugin <eugin.bernads@shellsquare.com>
2019-10-18 18:27:40 +0530

    Updated changes

--- js/trajectory.js
+++ js/trajectory.js
@@ -1,17 +1,17 @@
 var scene, camera, trajectorySurface;
 var geometry, material, mesh;
 var renderer;
+var height=10;
 
 init();
 
 function init() {
   trajectorySurface = document.getElementById("trajectory_3d");
   scene = new THREE.Scene();
-
   var aspectRatio = trajectorySurface.offsetWidth / trajectorySurface.offsetHeight;
   camera = new THREE.PerspectiveCamera(75, aspectRatio, .1, 1000);
+  // camera.lookAt(scene.position)
   scene.add(camera);
-
   renderer = new THREE.WebGLRenderer({
     canvas: trajectorySurface
   }, {
@@ -19,53 +19,119 @@ function init() {
   });
   renderer.setSize(trajectorySurface.offsetWidth, trajectorySurface.offsetHeight);
   renderer.setClearColor("#ddd", 1);
-
-  var controls = new THREE.OrbitControls(camera);
-  controls.mouseButtons = {
-    ORBIT: THREE.MOUSE.RIGHT,
-    ZOOM: THREE.MOUSE.MIDDLE,
-    PAN: THREE.MOUSE.LEFT
-  };
-
- 
+  var controls = new THREE.OrbitControls(camera, renderer.canvas);
+  controls;
   // Floor
-  var floorGeometry = new THREE.PlaneGeometry(80,100,0);
-  var floorMaterial = new THREE.MeshPhongMaterial({
-    color: "#E2B822",
-    shininess: 100
-  });
+  var floorGeometry = new THREE.PlaneGeometry(50, 50, 50);
+  var floorcolor = [
+    new THREE.MeshBasicMaterial({
+      color: "#E2B822",
+      transparent: true,
+      opacity: 0.3,
+      side: THREE.DoubleSide
+    }),
+    new THREE.MeshBasicMaterial({
+      color: "#E2B822",
+      transparent: true,
+      opacity: 5,
+      side: THREE.DoubleSide
+    }),
+
+  ]
+  var floorMaterial = new THREE.MeshFaceMaterial(floorcolor);
   var floor = new THREE.Mesh(floorGeometry, floorMaterial);
-  floor.rotation.x = -0.5 * Math.PI;
-  floor.position.x=10;
+  floor.rotation.x = Math.PI / 2;
+  floor.rotation.y = 0;
+  floor.rotation.z = Math.PI / Math.cos(270) * 0.492;
+  floor.position.set(25, 0, 25);
   floor.receiveShadow = true;
   scene.add(floor);
-  var spotLight = new THREE.SpotLight(0xffffff);
-  spotLight.position.set(15, 30, 50);
-  // spotLight.castShadow = true;
-  scene.add(spotLight);
-  let ambientLight = new THREE.AmbientLight(0x0c0c0c);
-  scene.add(ambientLight);
-  var light = new THREE.PointLight(0xffffff, 0.5);
-  light.position.set(20, 20, 50);
-  scene.add(light);
+
+  //floor2
+  var floorcolor1 = [
+    new THREE.MeshBasicMaterial({
+      color: "#356AE8",
+      transparent: true,
+      opacity: 0.5,
+      side: THREE.DoubleSide
+    }),
+    new THREE.MeshBasicMaterial({
+      color: "#356AE8	",
+      transparent: true,
+      opacity: 0.5,
+      side: THREE.DoubleSide
+    }),
+
+  ]
+  var floorMaterial1 = new THREE.MeshFaceMaterial(floorcolor1);
+  var floor1 = new THREE.Mesh(floorGeometry, floorMaterial1);
+  floor1.rotation.x = Math.PI / 2;
+  floor1.rotation.y = 0;
+  floor1.rotation.z = Math.PI / Math.cos(270) * 0.492;
+  floor1.position.set(25, height, 25);
+  floor1.receiveShadow = true;
+  scene.add(floor1);
+
+   //floor4
+   var floorcolor3 = [
+    new THREE.MeshBasicMaterial({
+      color: "#6AA121",
+      transparent: true,
+      opacity: 0.5,
+      side: THREE.DoubleSide
+    }),
+    new THREE.MeshBasicMaterial({
+      color: "#6AA121",
+      transparent: true,
+      opacity: 0.5,
+      side: THREE.DoubleSide
+    }),
+
+  ]
+  var floorMaterial3 = new THREE.MeshFaceMaterial(floorcolor3);
+  var floor3 = new THREE.Mesh(floorGeometry, floorMaterial3);
+  floor3.rotation.x = Math.PI / 2;
+  floor3.rotation.y = 0;
+  floor3.rotation.z = Math.PI / Math.cos(270) * 0.492;
+  var y1= height+20;
+  floor3.position.set(25, y1, 25);
+  floor3.receiveShadow = true;
+  scene.add(floor3);
+
+  var floorcolor2 = [
+    new THREE.MeshBasicMaterial({
+      color: "#C88141",
+      transparent: true,
+      opacity: 0.5,
+      side: THREE.DoubleSide
+    }),
+    new THREE.MeshBasicMaterial({
+      color: "#C88141",
+      transparent: true,
+      opacity: 0.5,
+      side: THREE.DoubleSide
+    }),
+
+  ]
+  var floorMaterial2 = new THREE.MeshFaceMaterial(floorcolor2);
+  var floor2 = new THREE.Mesh(floorGeometry, floorMaterial2);
+  floor2.rotation.x = Math.PI / 2;
+  floor2.rotation.y = 0;
+  floor2.rotation.z = Math.PI / Math.cos(270) * 0.492;
+  var y= height+10;
+  floor2.position.set(25, y, 25);
+  floor2.receiveShadow = true;
+  scene.add(floor2);
   var ah = new THREE.AxesHelper(50);
   ah.position.y -= 0.1; // The axis helper should not intefere with the grid helper
   scene.add(ah);
-  camera.position.x = 20;
-  camera.position.y = 30;
-  camera.position.z = 70;
+  camera.position.x = 10;
+  camera.position.y = 10;
+  camera.position.z = 100;
   render();
 };
 
 function render() {
-  var controls = new THREE.OrbitControls(camera);
-  controls.enableDamping = true; // For that slippery Feeling
-  controls.dampingFactor = 0.12; // Needs to call update on render loop 
-  controls.rotateSpeed = 0.001; // Rotate speed
-  controls.autoRotate = false; // turn this guy to true for a spinning camera
-  controls.autoRotateSpeed = 0.001; // 30
-  controls.maxPolarAngle = Math.PI / 2; // Don't let to go below the ground
   requestAnimationFrame(render);
-  controls.update();
   renderer.render(scene, camera);
 }
